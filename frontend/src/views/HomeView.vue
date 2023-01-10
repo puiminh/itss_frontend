0987c1342856c5fe126ba0cbfdb31d089078a00c
volmicrolab <volmicrolab@gmail.com>
2023-01-10 11:07:27 +0700

    some media query

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -20,7 +20,7 @@ export default {
     <div class="rounded-full bg-white w-fit py-0.5 px-2">
       <span class="font-bold text-sm text-gray-500">RECENT COURSE</span>
     </div>
-    <div class="grid grid-cols-6 gap-4 pt-5">
+    <div class="grid 2xl:grid-cols-6 gap-4 pt-5 md:grid-cols-3">
       <CourseFolder></CourseFolder>
       <CourseFolder></CourseFolder>
       <CourseFolder></CourseFolder>
@@ -30,7 +30,7 @@ export default {
     </div>
   </div>
 
-  <div class="flex gap-8">
+  <div class="flex gap-8 2xl:flex-row md:flex-col ">
     <div class="recommendCourseWrap rounded-3xl moreGrayBG p-4 mt-12 w-fit h-fit pb-8">
       <div class="rounded-full bg-white w-fit py-0.5 px-2">
         <span class="font-bold text-sm text-gray-500">MAYBE YOU WILL LIKE THIS</span>
@@ -51,10 +51,12 @@ export default {
       </div>
       <div class="grid grid-cols-2 gap-4 pt-5">
         <CourseFolder></CourseFolder>
-        <CourseFolder></CourseFolder>
-        <CourseFolder></CourseFolder>
-        <CourseFolder></CourseFolder>
-        <CourseFolder></CourseFolder>
+<div class="flex -space-x-4">
+    <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+    <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+    <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+    <a class="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
+</div>
       </div>
     </div>
 
