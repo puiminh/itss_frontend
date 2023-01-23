d6fb9542d148612e252beea66c6ebf1eb939ed46
puiminh <volmicrolab@gmail.com>
2023-01-23 23:07:49 +0700

    add debouncing for input

--- frontend/src/components/search/Search.vue
+++ frontend/src/components/search/Search.vue
@@ -7,7 +7,9 @@
   			    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
 		    </svg>
         </div>
-        <input type="text" id="simple-search" 
+        <input 
+            @keyup="handleInput"
+            type="text" id="simple-search" 
             class=" font-mono text-md text-gray-500
             bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
     </div>
@@ -49,6 +51,20 @@ export default {
     },
     data() {
         return {
+            timeOut: null,
+        }
+    },
+    methods: {
+        handleInput() {
+            console.log('Handle input');
+            clearTimeout(this.timeOut);
+
+            this.timeOut = setTimeout(() => {
+                this.callAPI();
+            }, 500);
+        },
+        callAPI() {
+            console.log('call');
         }
     }
 }
